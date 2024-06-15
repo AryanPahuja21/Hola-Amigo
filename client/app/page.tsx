3ff38810b893b678a47c13da6eec7d8c7db74fa1
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-06-16 01:03:38 +0530

    logo and primary added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <p className="text-5xl text-sky-500 text-center font-bold my-7">
+      <p className="text-5xl text-transparent bg-clip-text bg-gradient-to-b from-blue-700 to bg-primary p-2 text-center font-bold my-7">
         Hola Amigo!
       </p>
     </main>
