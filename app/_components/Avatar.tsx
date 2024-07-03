172ccaaec896b9f53c7ed52a4fc0d9e46034ce50
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-04 02:40:22 +0530

    users list ready

--- app/_components/Avatar.tsx
+++ app/_components/Avatar.tsx
@@ -11,8 +11,8 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
   return (
     <div className="relative">
       <div className="relative inline-block overflow-hidden h-9 w-9">
-        <Image src={user?.image || "/avatar.jpg"} alt="Avatar" fill />
-        <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-1 w-1 md:h-3 md:w-3" />
+        <Image src={user?.image || "/avatar.png"} alt="Avatar" fill />
+        <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0.5 right-0.5 h-2 w-2" />
       </div>
     </div>
   );
