ef90a5ef1c0c7e36d55667f3c0470f9660f2ccef
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-13 03:33:06 +0530

    profile settings modal ui

---
+++ app/_components/sidebar/SettingsModal.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import Modal from "../Modal";
+import { useState } from "react";
+
+interface SettingsModalProps {
+  currentUser: User;
+  isOpen?: boolean;
+  onClose: () => void;
+}
+
+const SettingsModal: React.FC<SettingsModalProps> = ({
+  currentUser,
+  isOpen,
+  onClose,
+}) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: currentUser?.name,
+      image: currentUser?.image,
+    },
+  });
+
+  const image = watch("image");
+
+  const handleUpload = (result: any) => {
+    setValue("image", result?.info?.secure_url, { shouldValidate: true });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/settings", data)
+      .then(() => {
+        onClose();
+      })
+      .catch(() => toast.error("Failed to update settings"))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Profile
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Edit your public information
+            </p>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default SettingsModal;
