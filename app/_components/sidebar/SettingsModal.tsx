cb7e4f827d1031e99becb211a563b8c47e320b11
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-13 03:46:10 +0530

    settings modal ui done

--- app/_components/sidebar/SettingsModal.tsx
+++ app/_components/sidebar/SettingsModal.tsx
@@ -7,6 +7,10 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import Modal from "../Modal";
 import { useState } from "react";
+import Input from "../inputs/Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
 
 interface SettingsModalProps {
   currentUser: User;
@@ -64,6 +68,49 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
             <p className="mt-1 text-sm leading-6 text-gray-600">
               Edit your public information
             </p>
+            <div className="mt-10 flex flex-col gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <Image
+                    width="48"
+                    height="48"
+                    className="rounded-full"
+                    src={image || currentUser?.image || "/avatar.png"}
+                    alt="avatar"
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUploadAdded={handleUpload}
+                    uploadPreset="jyqv9xem"
+                  >
+                    <Button disabled={isLoading} secondary type="button">
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <Button disabled={isLoading} onClick={onClose} secondary>
+                Cancel
+              </Button>
+              <Button disabled={isLoading} type="submit">
+                Save
+              </Button>
+            </div>
           </div>
         </div>
       </form>
